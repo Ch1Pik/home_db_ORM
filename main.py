f425fa9714be2a0c036d59139c584ffe79634074
Sergey Novikov <149697882+Ch1Pik@users.noreply.github.com>
2024-02-10 13:07:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from models import create_tables, Publisher, Book, Shop, Stock, Sale
+from sqlalchemy.orm import sessionmaker
+from models import create_tables, Publisher, Shop, Sale, Book, Stock
 
 DSN = "postgresql://postgres:postgres@localhost:5432/home_db_ORM"
 engine = sq.create_engine(DSN)
 
 create_tables(engine)
-
-p1 = Publisher(name='Пушкин', books = [
-    Book(title="Евгений Онегин"),
-    Book(title="Сказка о царе Салтане"),
-    Book(title="Медный всадник")])
-p2 = Publisher(name='Глуховский', books = [
-    Book(title="Метро 2033"),
-    Book(title="Метро 2034"),
-    Book(title="Метро 2035")])
-b1 = Book(title="Текст", publisher=p2)
-b2 = Book(title="Будущее", publisher=p2)
-b3 = Book(title="Сказка о рыбаке и рыбке", publisher=p1)
-
-shop1 = Shop(name="Литрес")
-stock1 = Stock(book=b1, shop=shop1, count=30)
-sale1 = Sale(price=470, stock=stock1, count=20)
-stock2 = Stock(book=b3, shop=shop1, count=50)
-sale2 = Sale(price=320, stock=stock2, count=20)
-
-shop2 = Shop(name="Эксмо")
-stock3 = Stock(book=b2, shop=shop2, count=50)
-sale3 = Sale(price=510, stock=stock3, count=35)
-stock4 = Stock(book=b2, shop=shop2, count=40)
-sale4 = Sale(price=380, stock=stock4, count=15)
-
 Session = sessionmaker(bind=engine)
-s = Session()
-s.add_all([p1, p2, b1, b2, b3, shop1, shop2, stock1, stock2, stock3, stock4, sale1, sale2, sale3, sale4])
-s.commit()
+session = Session()
 
-def searching_publisher_name():
-    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
-    query_publisher_name = input('Введите имя (name) издателя: ')
-    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
-    for result in query_result.all():
-        print(f'Издатель "{query_publisher_name}" найден в магазине "{result.name}" с идентификатором {result.id}')
+publisher1 = Publisher(name='Пушкин')
+publisher2 = Publisher(name='Глуховский')
 
-def searching_publisher_id():
-    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
-    query_publisher_name = input('Введите идентификатор (id) издателя: ')
-    query_result = query_join.filter(Publisher.id_publisher == query_publisher_name)
-    for result in query_result.all():
-        print(
-            f'Издатель c id: {query_publisher_name} найден в магазине "{result.name}" '
-            f'с идентификатором {result.id}')
+session.add(publisher1)
+session.add(publisher2)
 
-if __name__ == '__main__':
-    searching_publisher_name()
-    searching_publisher_id()
+book1 = Book(title='Евгений Онегин', publisher=publisher1)
+book2 = Book(title='Сказка о царе Салтане', publisher=publisher1)
+book3 = Book(title='Медный всадник', publisher=publisher1)
+book4 = Book(title='Метро 2033', publisher=publisher2)
+book5 = Book(title='Метро 2034', publisher=publisher2)
+book6 = Book(title='Метро 2035', publisher=publisher2)
+session.add_all([book1, book2, book3, book4, book5, book6])
 
-s.close()
+
+shop1 = Shop(name='Литрес')
+shop2 = Shop(name='Эксмо')
+session.add_all([shop1, shop2])
+
+
+stock1 = Stock(book=book1, shop=shop2, count=10)
+stock2 = Stock(book=book3, shop=shop2, count=5)
+stock3 = Stock(book=book2, shop=shop1, count=1)
+session.add_all([stock3, stock2, stock1])
+
+
+sale1 = Sale(price='100.8', data_sale="2023-12-25T15:43:24.552Z", stock=stock1, count=2)
+sale2 = Sale(price='50.5', data_sale="2024-01-25T12:46:24.552Z", stock=stock1, count=4)
+session.add_all([sale1, sale2])
+session.commit()
+
+session.close()
+
+
+def publisher_data(class_):
+    id_ = input('Введите id издателя:')
+    name = input('Введите name издателя:')
+    if id_:
+        for c in session.query(class_).filter(class_.id == id_).all():
+            print(c)
+
+    elif name:
+        for c in session.query(class_).filter(class_.name == name).all():
+            print(c)
+
+
+def publisher_shop(class_):
+    id_ = input('Введите id издателя:')
+    name = input('Введите name издателя:')
+    query = session.query(Shop)
+    query = query.join(Stock)
+    query = query.join(Book)
+    query = query.join(Publisher)
+    if id:
+        for c in query.filter(class_.id == id_).all():
+            print(c)
+
+    elif name:
+        for c in query.filter(class_.name == name).all():
+            print(c)
+
+
+if __name__ == "__main__":
+    publisher_data(Publisher)
+    publisher_shop(Publisher)
