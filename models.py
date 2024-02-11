dc864baf6428756574c60657e87c78ce8f2bf315
Sergey Novikov <149697882+Ch1Pik@users.noreply.github.com>
2024-02-11 11:40:47 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,64 +1,74 @@
+import json
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
+
 class Publisher(Base):
     __tablename__ = "publisher"
+
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-
-    def __str__(self):
-        return f'id{self.id}:Name {self.name}'
+    name = sq.Column(sq.String(length=40), nullable=False)
 
 
 class Book(Base):
     __tablename__ = "book"
+
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=40), nullable=False)
-    id_publisher =sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
-    publisher = relationship(Publisher, backref="books")
-
-    def __str__(self):
-        return f'id {self.id}:Title {self.title}:Id_publisher {self.id_publisher}'
+    publisher = relationship(Publisher, backref="publisher")
 
 
 class Shop(Base):
     __tablename__ = "shop"
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
 
-    def __str__(self):
-        return f'id {self.id}:Name_shop {self.name}'
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), nullable=False)
+
 
 class Stock(Base):
     __tablename__ = "stock"
+
     id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer,sq.ForeignKey("book.id"), nullable=False)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    count = sq.Column(sq.Integer)
+    count = sq.Column(sq.Integer, nullable=False)
 
+    book = relationship(Book, backref="book")
+    shop = relationship(Shop, backref="shop")
 
-    book = relationship(Book, backref="stocks")
-    shop = relationship(Shop, backref='shops')
-    def __def__(self):
-        return f'id {self.id}:id_book{self.id_book}:id_shop{self.id_shop}:count{self.count}'
 
 class Sale(Base):
     __tablename__ = "sale"
-    id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Numeric, nullable=False)
-    data_sale = sq.Column(sq.DateTime)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"))
-    count = sq.Column(sq.Integer)
-    stock = relationship(Stock, backref="sales")
-    def __str__(self):
-        return f'id{self.id}:prise{self.prise}:data_sale{self.data_sale}:id_stok{self.id_stock}:count{self.id_stock}'
 
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+
+    stock = relationship(Stock, backref="stock")
 
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+
+def add_json(session):
+    with open('data.json', 'r') as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
